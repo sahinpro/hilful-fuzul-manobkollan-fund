952bb40453bc5aba0e2b94d87c5c928660b38ea7
sahinhub <sahinhub@gmail.com>
2026-05-24 22:40:22 +0600

    cursor pointer on all action button

--- supabase/migrations/20260525130000_donation_intents_add_rocket.sql
+++
@@ -1,8 +0,0 @@
--- Allow Rocket on semi-auto donation intents (run after 20260525120000).
-
-ALTER TABLE public.donation_intents
-  DROP CONSTRAINT IF EXISTS donation_intents_payment_method_check;
-
-ALTER TABLE public.donation_intents
-  ADD CONSTRAINT donation_intents_payment_method_check
-  CHECK (payment_method IN ('bkash', 'nagad', 'rocket'));
